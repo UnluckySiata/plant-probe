9f522340df2b191ed4bdc64fc29c2f7695683e02
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2024-05-10 13:15:34 +0200

    test embassy rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+#![allow(unused_imports)]
 #![no_std]
 #![no_main]
 
-use bsp::entry;
-use embedded_hal::digital::v2::OutputPin;
-use panic_halt as _;
+use embassy_executor::Spawner;
+use embassy_rp::adc::{self, Adc, Channel};
+use embassy_rp::bind_interrupts;
+use embassy_rp::gpio::{self, AnyPin, Flex, Level, Output, OutputOpenDrain, Pull};
+use embassy_rp::i2c;
+use embassy_rp::peripherals::USB;
+use embassy_rp::usb::{self, Driver, InterruptHandler};
+use embassy_time::{Delay, Timer};
+use embedded_hal_1::digital::{OutputPin, StatefulOutputPin};
+use {defmt_rtt as _, panic_probe as _};
 
 use embedded_graphics::{
     mono_font::{ascii::FONT_8X13, MonoTextStyleBuilder},
@@ -19,65 +27,38 @@ use one_wire_bus::{self, Address};
 use core::fmt::Write;
 use heapless::String;
 
-// Provide an alias for our BSP so we can switch targets quickly.
-use rp_pico as bsp;
-
-use bsp::hal::{
-    adc::{Adc, AdcPin},
-    clocks::ClockSource,
-    fugit::RateExtU32,
-    gpio::{FunctionI2C, Pin},
-    pac,
-};
-
-const XTAL_FREQ_HZ: u32 = 12_000_000u32;
-
 #[derive(Debug)]
 enum Err {}
 
-#[entry]
-fn main() -> ! {
-    let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = bsp::hal::Watchdog::new(pac.WATCHDOG);
-    let sio = bsp::hal::sio::Sio::new(pac.SIO);
+bind_interrupts!(struct Irqs {
+    USBCTRL_IRQ => usb::InterruptHandler<USB>;
+    ADC_IRQ_FIFO => adc::InterruptHandler;
+});
 
-    let clocks = bsp::hal::clocks::init_clocks_and_plls(
-        XTAL_FREQ_HZ,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .unwrap();
+#[embassy_executor::task]
+async fn logger_task(driver: Driver<'static, USB>) {
+    embassy_usb_logger::run!(1024, log::LevelFilter::Info, driver);
+}
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.get_freq().to_Hz());
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    let driver = Driver::new(p.USB, Irqs);
+    spawner.spawn(logger_task(driver)).unwrap();
 
-    let pins = bsp::hal::gpio::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
+    let mut delay = Delay;
 
-    let mut led = pins.gpio28.into_push_pull_output();
-    led.set_high().unwrap();
+    let mut adc = Adc::new(p.ADC, Irqs, adc::Config::default());
 
-    // pins for oled i2c display
-    let sda_pin: Pin<_, FunctionI2C, _> = pins.gpio0.reconfigure();
-    let scl_pin: Pin<_, FunctionI2C, _> = pins.gpio1.reconfigure();
+    let mut adc_pin_0 = Channel::new_pin(p.PIN_26, Pull::None);
+    let mut adc_pin_1 = Channel::new_pin(p.PIN_27, Pull::None);
+    let mut ts = Channel::new_temp_sensor(p.ADC_TEMP_SENSOR);
 
-    let i2c = bsp::hal::I2C::i2c0(
-        pac.I2C0,
-        sda_pin,
-        scl_pin,
-        400.kHz(),
-        &mut pac.RESETS,
-        &clocks.system_clock,
-    );
+    let sda_pin = p.PIN_0;
+    let scl_pin = p.PIN_1;
+    let mut led = Output::new(p.PIN_21, Level::Low);
 
+    let i2c = i2c::I2c::new_blocking(p.I2C0, scl_pin, sda_pin, i2c::Config::default());
     let interface = I2CDisplayInterface::new(i2c);
     let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
@@ -91,7 +72,13 @@ fn main() -> ! {
 
     let mut str: String<1024> = String::new();
 
-    let ow_pin = pins.gpio22.into_push_pull_output();
+    Text::with_baseline("fdashfasd", Point::zero(), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+    display.flush().unwrap();
+
+    // let ow_pin = Flex::new(p.PIN_22);
+    let ow_pin = OutputOpenDrain::new(p.PIN_22, Level::Low);
     let mut one_wire_bus = one_wire_bus::OneWire::new(ow_pin).unwrap();
 
     let mut addr = Address(0x0);
@@ -101,43 +88,51 @@ fn main() -> ! {
             continue;
         }
 
+        log::info!("fdsafd");
         addr = device_address;
         break;
     }
+    //
+    // let sensor = Ds18b20::new::<Err>(addr).unwrap();
 
-    let mut adc = Adc::new(pac.ADC, &mut pac.RESETS);
-    let adc_pin_0 = AdcPin::new(pins.gpio27.into_floating_input()).unwrap();
-    adc.free_running(&adc_pin_0);
-    adc.wait_ready();
-
-    let sensor = Ds18b20::new::<Err>(addr).unwrap();
-
-    let mut led_on = true;
+    let mut led_on = false;
+    Timer::after_secs(5).await;
     loop {
-        delay.delay_ms(1000);
-
-        if led_on {
-            led.set_low().unwrap();
-        } else {
-            led.set_high().unwrap();
-        }
-        led_on = !led_on;
+        log::info!("addr: {:?}", addr);
+        Timer::after_secs(1).await;
 
         str.clear();
         display.clear(BinaryColor::Off).unwrap();
 
-        sensor
-            .start_temp_measurement(&mut one_wire_bus, &mut delay)
-            .unwrap();
-        let data = sensor.read_data(&mut one_wire_bus, &mut delay).unwrap();
-        writeln!(&mut str, "Temp - {:.3} C", data.temperature).unwrap();
+        if led_on {
+            led.set_low();
+        } else {
+            led.set_high();
+        }
+        led_on = !led_on;
 
-        let counts = adc.read_single();
-        writeln!(&mut str, "Value: {}", counts).unwrap();
+        let temp = adc.read(&mut ts).await.unwrap();
+        writeln!(&mut str, "Temp: {} degrees", convert_to_celsius(temp)).unwrap();
+        let level = adc.read(&mut adc_pin_0).await.unwrap();
+        writeln!(&mut str, "Adc0: {}", level).unwrap();
+        let level = adc.read(&mut adc_pin_1).await.unwrap();
+        writeln!(&mut str, "Adc1: {}", level).unwrap();
+        // sensor
+        //     .start_temp_measurement(&mut one_wire_bus, &mut delay)
+        //     .unwrap();
+        // let data = sensor.read_data(&mut one_wire_bus, &mut delay).unwrap();
+        // writeln!(&mut str, "Temp - {:.3} C", data.temperature).unwrap();
 
         Text::with_baseline(&str, Point::zero(), text_style, Baseline::Top)
             .draw(&mut display)
             .unwrap();
         display.flush().unwrap();
     }
+}
+
+fn convert_to_celsius(raw_temp: u16) -> f32 {
+    let temp = 27.0 - (raw_temp as f32 * 3.3 / 4096.0 - 0.706) / 0.001721;
+    let sign = if temp < 0.0 { -1.0 } else { 1.0 };
+    let rounded_temp_x10: i16 = ((temp * 10.0) + 0.5 * sign) as i16;
+    (rounded_temp_x10 as f32) / 10.0
 }
