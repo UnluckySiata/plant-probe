1e94e829d2a4c1c8f8a0d91d4d36de7d1b626555
Mikołaj Siatkowski <msiatkowski@tutanota.com>
2024-05-12 00:19:42 +0200

    switch to embassy

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-#![allow(unused_imports)]
 #![no_std]
 #![no_main]
 
 use embassy_executor::Spawner;
 use embassy_rp::adc::{self, Adc, Channel};
 use embassy_rp::bind_interrupts;
-use embassy_rp::gpio::{self, AnyPin, Flex, Level, Output, OutputOpenDrain, Pull};
+use embassy_rp::gpio::{Level, Output, Input, OutputOpenDrain, Pull};
 use embassy_rp::i2c;
 use embassy_rp::peripherals::USB;
-use embassy_rp::usb::{self, Driver, InterruptHandler};
+use embassy_rp::usb::{self, Driver};
 use embassy_time::{Delay, Timer};
-use embedded_hal_1::digital::{OutputPin, StatefulOutputPin};
 use {defmt_rtt as _, panic_probe as _};
 
 use embedded_graphics::{
-    mono_font::{ascii::FONT_8X13, MonoTextStyleBuilder},
+    mono_font::{ascii::FONT_6X9, MonoTextStyleBuilder},
     pixelcolor::BinaryColor,
     prelude::*,
     text::{Baseline, Text},
@@ -29,6 +27,8 @@ use heapless::String;
 
 #[derive(Debug)]
 enum Err {}
+
+const ADC_MAX: u16 = 4096;
 
 bind_interrupts!(struct Irqs {
     USBCTRL_IRQ => usb::InterruptHandler<USB>;
@@ -52,7 +52,10 @@ async fn main(spawner: Spawner) {
 
     let mut adc_pin_0 = Channel::new_pin(p.PIN_26, Pull::None);
     let mut adc_pin_1 = Channel::new_pin(p.PIN_27, Pull::None);
+    let mut adc_pin_2 = Channel::new_pin(p.PIN_28, Pull::None);
     let mut ts = Channel::new_temp_sensor(p.ADC_TEMP_SENSOR);
+
+    let btn_pin = Input::new(p.PIN_20, Pull::Up);
 
     let sda_pin = p.PIN_0;
     let scl_pin = p.PIN_1;
@@ -66,18 +69,12 @@ async fn main(spawner: Spawner) {
     display.init().unwrap();
 
     let text_style = MonoTextStyleBuilder::new()
-        .font(&FONT_8X13)
+        .font(&FONT_6X9)
         .text_color(BinaryColor::On)
         .build();
 
     let mut str: String<1024> = String::new();
 
-    Text::with_baseline("fdashfasd", Point::zero(), text_style, Baseline::Top)
-        .draw(&mut display)
-        .unwrap();
-    display.flush().unwrap();
-
-    // let ow_pin = Flex::new(p.PIN_22);
     let ow_pin = OutputOpenDrain::new(p.PIN_22, Level::Low);
     let mut one_wire_bus = one_wire_bus::OneWire::new(ow_pin).unwrap();
 
@@ -88,46 +85,53 @@ async fn main(spawner: Spawner) {
             continue;
         }
 
-        log::info!("fdsafd");
         addr = device_address;
         break;
     }
-    //
-    // let sensor = Ds18b20::new::<Err>(addr).unwrap();
+    let sensor = Ds18b20::new::<Err>(addr).unwrap();
 
-    let mut led_on = false;
-    Timer::after_secs(5).await;
     loop {
-        log::info!("addr: {:?}", addr);
         Timer::after_secs(1).await;
 
         str.clear();
         display.clear(BinaryColor::Off).unwrap();
 
-        if led_on {
-            led.set_low();
-        } else {
-            led.set_high();
+        match btn_pin.get_level() {
+            Level::Low => {
+                led.set_high();
+            }
+            Level::High => {
+                led.set_low();
+            }
         }
-        led_on = !led_on;
+
+        let level = adc.read(&mut adc_pin_0).await.unwrap();
+        writeln!(&mut str, "Adc0: {:.2}", adc_ratio(level)).unwrap();
+
+        let level = adc.read(&mut adc_pin_1).await.unwrap();
+        writeln!(&mut str, "Adc1: {:.2}", adc_ratio(level)).unwrap();
+
+        let level = adc.read(&mut adc_pin_2).await.unwrap();
+        writeln!(&mut str, "Adc2: {:.2}", adc_ratio(level)).unwrap();
 
         let temp = adc.read(&mut ts).await.unwrap();
-        writeln!(&mut str, "Temp: {} degrees", convert_to_celsius(temp)).unwrap();
-        let level = adc.read(&mut adc_pin_0).await.unwrap();
-        writeln!(&mut str, "Adc0: {}", level).unwrap();
-        let level = adc.read(&mut adc_pin_1).await.unwrap();
-        writeln!(&mut str, "Adc1: {}", level).unwrap();
-        // sensor
-        //     .start_temp_measurement(&mut one_wire_bus, &mut delay)
-        //     .unwrap();
-        // let data = sensor.read_data(&mut one_wire_bus, &mut delay).unwrap();
-        // writeln!(&mut str, "Temp - {:.3} C", data.temperature).unwrap();
+        writeln!(&mut str, "Temp inside: {:.3}", convert_to_celsius(temp)).unwrap();
+
+        sensor
+            .start_temp_measurement(&mut one_wire_bus, &mut delay)
+            .unwrap();
+        let data = sensor.read_data(&mut one_wire_bus, &mut delay).unwrap();
+        writeln!(&mut str, "Temp outside: {:.3}", data.temperature).unwrap();
 
         Text::with_baseline(&str, Point::zero(), text_style, Baseline::Top)
             .draw(&mut display)
             .unwrap();
         display.flush().unwrap();
     }
+}
+
+fn adc_ratio(raw_read: u16) -> f64 {
+    raw_read as f64 / ADC_MAX as f64
 }
 
 fn convert_to_celsius(raw_temp: u16) -> f32 {
